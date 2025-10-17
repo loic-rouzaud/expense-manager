d13aeab50db7a7474efa05bb4e471f7178779a57
loic-rouzaud <loic.rouzaud@epitech.eu>
2025-10-17 13:51:21 +0200

    [RM] - useless imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 interface ApiResponse {
@@ -37,17 +35,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
       <div className="card">
         <button onClick={helloworld}>Appeler le backend</button>
 
@@ -57,10 +44,6 @@ function App() {
           </pre>
         )}
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
